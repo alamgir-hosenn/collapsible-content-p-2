bfe595734fc8ff576c5ee3770ccc46450e75bef7
alamgir_hosen <thisisalamgir@gmail.com>
2024-08-10 20:02:23 +0600

    redesigned & added icons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let collapsible = document.getElementById("collapsible");
-let content = document.getElementById("content");
+let collapsible_content = document.getElementById("collapsible_content");
 
 collapsible.addEventListener("click", () => {
-  content.classList.toggle("content_show");
+  collapsible.classList.toggle("active");
+  collapsible_content.classList.toggle("show_content");
 });
